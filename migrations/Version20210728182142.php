3dc06fa858367ef19726a77bc6bd234deb5e8171
Ivan Puzatkin <ivan.puzatkin@chaport.com>
2021-07-28 21:34:56 +0300

    add advert entity

---
+++ migrations/Version20210728182142.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210728182142 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE advert (id INT AUTO_INCREMENT NOT NULL, text VARCHAR(255) NOT NULL, price DECIMAL(10, 2) NOT NULL, show_count INT UNSIGNED NOT NULL DEFAULT 0, show_limit INT UNSIGNED NOT NULL, banner VARCHAR(255) NOT NULL, PRIMARY KEY(id), INDEX(price)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE advert');
+    }
+}
---
+++ src/Entity/Advert.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AdvertRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=AdvertRepository::class)
+ */
+class Advert
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $text;
+
+    /**
+     * @ORM\Column(type="decimal", precision=10, scale=2)
+     */
+    private $price;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $show_count;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $show_limit;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $banner;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getText(): ?string
+    {
+        return $this->text;
+    }
+
+    public function setText(string $text): self
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    public function getPrice(): string
+    {
+        return $this->price;
+    }
+
+    public function setPrice(string $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getShowLimit(): int
+    {
+        return $this->show_limit;
+    }
+
+    public function setShowLimit(int $show_limit): self
+    {
+        $this->show_limit = $show_limit;
+
+        return $this;
+    }
+
+    public function getShowCount(): int
+    {
+        return $this->show_count;
+    }
+
+    public function setShowCount(int $show_count): self
+    {
+        $this->show_count = $show_count;
+
+        return $this;
+    }
+
+    public function getBanner(): string
+    {
+        return $this->banner;
+    }
+
+    public function setBanner(string $banner): self
+    {
+        $this->banner = $banner;
+
+        return $this;
+    }
+}
